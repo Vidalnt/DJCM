b1d3bd54445f4689cb293396ac81107924e68e3a
vidalnt <raddiscord1@gmail.com>
2025-10-07 20:30:45 -0500

    Add Vocadito

---
+++ dataset/pre_Vocadito.py
@@ -0,0 +1,33 @@
+import os
+import pandas as pd
+from tqdm import tqdm
+import librosa
+import soundfile as sf
+import numpy as np
+
+df_info = pd.read_csv(r'D:\ICASSP_2024\SVSDT\dataset\INFO\vocadito.csv')
+sr = 16000
+path_in = r'D:\Dataset\Vocadito'
+path_out = r'D:\ICASSP_2024\SVSDT\dataset\Vocadito'
+
+for _, row in tqdm(df_info.iterrows(), total=len(df_info)):
+    filename, label_path, split = row['name'], row['label_path'], row['split']
+   
+    audio_m, _ = librosa.load(os.path.join(path_in, filename), sr=sr, mono=True)
+   
+    os.makedirs(os.path.join(path_out, split), exist_ok=True)
+   
+    out_filename = os.path.splitext(os.path.basename(filename))[0]
+    sf.write(os.path.join(path_out, split, f"{out_filename}_m.wav"),
+             audio_m.T, sr, 'PCM_24')
+   
+    pv_in = os.path.join(path_in, label_path)
+    pv_out = os.path.join(path_out, split, f"{out_filename}.pv")
+   
+    f0_df = pd.read_csv(pv_in)
+    f0 = f0_df['frequency'].values
+   
+    f0 = np.nan_to_num(f0)
+    f0[f0 < 0] = 0.0
+   
+    np.savetxt(pv_out, f0, fmt="%.9f")
